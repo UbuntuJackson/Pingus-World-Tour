14f0590fad4c0d5ec4fc1e15f8778a77640dc60e
UbuntuJackson <46799187+UbuntuJackson@users.noreply.github.com>
2023-11-24 18:41:43 +0100

    added variable called target id in pingus level

--- src/pingus_level.h
+++ src/pingus_level.h
@@ -8,6 +8,7 @@ class PingusWorldTour;
 class PingusLevel : public CellMap{
 public:
     int actor_id_count;
+    int target_id = -1;
     PingusWorldTour* game;
     PingusLevel(PingusWorldTour* _game);
     void NewActor(std::string _actor, int _x, int _y, std::string _layer);
